61d01c49b923df2d1b883842be342c1700c68d80
ravivemula123799 <ravikumarvemula74@gmail.com>
2025-05-07 16:21:48 +0530

    Update vem.py

--- vem.py
+++ vem.py
@@ -1,10 +1,9 @@
-# Function to calculate factorial using a loop
+
 def factorial(n):
     result = 1
     for i in range(1, n + 1):
         result *= i
     return result
 
-# Example usage:
 num = int(input("Enter a number: "))
 print(f"The factorial of {num} is {factorial(num)}")
